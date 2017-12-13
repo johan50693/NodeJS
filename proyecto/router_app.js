f24562bb341f05b0dabbffb165f54fe7112cf2e1
Albert <johan50693@gmail.com>
2017-12-13 17:02:21 -0400

    Se cargan la imagenes en el index

--- proyecto/router_app.js
+++ proyecto/router_app.js
@@ -12,9 +12,15 @@ var fs= require("fs");
 // var upload= multer({dest: 'uploads/'});
 
 
-router.get("",function(req,res){
+router.get("/",function(req,res){
 
-	res.render("app/home");
+	Imagen.find({})
+		  .populate("creator")
+		  .exec(function(err,imagenes){
+		  	if(err) console.log(err);
+			res.render("app/home",{imagenes: imagenes});	  	
+		  })
+	
 });
 
 router.get("/imagenes/new",function(req,res){
